919afd20057dd6cced458b6cb3478b6fb458fe94
cec61438-dotcom <cec61438@gmail.com>
2026-09-10 19:18:25 -0700

    Add files via upload

---
+++ Bab 2/program_pertama.py
@@ -0,0 +1,10 @@
+# Program Pertamaku
+# Nama: [tulis namamu]
+# Kelas: X RPL
+print("====================================")
+print("     SELAMAT DATANG DI PYTHON!")
+print("====================================")
+print()
+print("Program ini dibuat oleh:", "[Nana]")
+print("Sekolah: SMK TJP Tuban")
+print("Tahun :", 2026)
